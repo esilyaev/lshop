e9a06a9189cb5b5368016219298e669246c859cf
Ilyae <johndoe@example.com>
2022-08-21 23:59:46 +0300

    add user

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -27,6 +27,15 @@ class DatabaseSeeder extends Seeder
         'remember_token' => Str::random(10),
       ]
     );
+    User::factory()->create(
+      [
+        'name' => 'il',
+        'email' => 'sil@bk.com',
+        'email_verified_at' => now(),
+        'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
+        'remember_token' => Str::random(10),
+      ]
+    );
 
     BlogCategory::factory(10)->create();
     BlogRecord::factory(250)->create();
